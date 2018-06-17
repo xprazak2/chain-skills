cf9bdb0da92ac6f628aea8610be2b516153a98f6
Ondřej Pražák <o.prazak5@seznam.cz>
2018-06-17 13:56:16 +0200

    initial commit

---
+++ contracts/ChainList.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.18;
+
+contract ChainList {
+  address seller;
+  string name;
+  string description;
+  uint256 price;
+
+  function sellArticle(string _name, string _description, uint256 _price) public {
+    seller = msg.sender;
+    name = _name;
+    description = _description;
+    price = _price;
+  }
+
+  function getArticle() public view returns (
+    address _seller,
+    string _name,
+    string _description,
+    uint256 _price ) {
+    return (seller, name, description, price);
+  }
+}
